5bcdf673f59ff14e55bb71f0d7b06352a9b32c53
slemerdy <sebastian.lemerdy@speach.me>
2016-11-24 14:04:19 +0100

    first basic implementation
    reminds CRA
    special reply to marion and rose :)
    responds when cited

---
+++ src/main/java/speachmebot/SpeachMeBot.java
@@ -0,0 +1,68 @@
+package speachmebot;
+
+import com.ullink.slack.simpleslackapi.SlackSession;
+import com.ullink.slack.simpleslackapi.impl.SlackSessionFactory;
+
+import java.io.IOException;
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import static java.time.ZoneId.systemDefault;
+
+public class SpeachMeBot {
+
+    public static final int ONE_WEEK_MILLISECONDS = 1000 * 60 * 60 * 24 * 7;
+
+    public static void main(String[] args) {
+        SlackSession session = SlackSessionFactory.createWebSocketSlackSession(args[0]);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
+                session.disconnect();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }));
+
+        try {
+            session.connect();
+
+            session.addMessagePostedListener(new EventsPosted());
+
+            scheduleAtFixedRate(DayOfWeek.FRIDAY, 14, 0, ONE_WEEK_MILLISECONDS, () ->
+                    session.getChannels().stream()
+                            .filter(channel -> "magicians".equals(channel.getName()))
+                            .findFirst()
+                            .ifPresent(magicians -> session.sendMessage(magicians, "pensez à vos CRA les gars"))
+            );
+
+            while (true) {
+                Thread.sleep(Long.MAX_VALUE);
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                session.disconnect();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void scheduleAtFixedRate(DayOfWeek dayOfWeek, int hour, int minutes, int period, Runnable task) {
+        LocalDateTime now = LocalDateTime.now();
+        int deltaDaysToFriday = (dayOfWeek.getValue() - now.getDayOfWeek().getValue()) % 7;
+        LocalDateTime nextFriday = now.plusDays(deltaDaysToFriday).withHour(hour).withMinute(minutes).withSecond(0).withNano(0);
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                task.run();
+            }
+        }, Date.from(nextFriday.atZone(systemDefault()).toInstant()), period);
+    }
+
+}
